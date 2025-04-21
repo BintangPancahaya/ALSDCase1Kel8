be24ec38ff3b2bf328e261abd05c0957cabbe4a7
Nadyaaurora <nadyaaurora575@gmail.com>
2025-04-21 15:03:51 +0700

    feat : class matkul

---
+++ Matkul.java
@@ -0,0 +1,15 @@
+public class Matkul {
+    String kodeMK, namaMK;
+    int sks;
+
+    public Matkul(String kodeMK, String namaMK, int sks) {
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+        this.sks = sks;
+    }
+
+    void tampilMatkul() {
+        System.out.println("Kode MK : " + kodeMK);
+        System.out.println("Nama MK : " + namaMK);
+    }
+}
